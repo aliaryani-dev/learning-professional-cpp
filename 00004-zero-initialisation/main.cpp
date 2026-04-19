817ae0a0fa0c2e1ea38fdffb402597c95b46c9e3
Ali Aryani <lenesis@proton.me>
2026-04-19 18:11:59 +0300

    zero initialisation

---
+++ 00004-zero-initialisation/main.cpp
@@ -0,0 +1,15 @@
+#include <print>
+using namespace std;
+
+int main() {
+    float my_float {}; // initialised to zero
+    float my_second_float; // random data inside
+    int my_int {};
+    int my_second_int;
+    println("my_float : {}", my_float);
+    println("my_second_float : {}", my_second_float);
+    println("my_int : {}",my_int);
+    println("my_second_int : {}",my_second_int);
+
+    return 0;
+}
