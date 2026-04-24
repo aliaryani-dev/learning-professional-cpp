01631acaf096ec9b8b9b1fe06e0783d4c2794c06
Ali Aryani <lenesis@proton.me>
2026-04-24 08:38:12 +0330

    switch statement

--- 00008-conditional-statements/main.cpp
+++ 00008-conditional-statements/main.cpp
@@ -9,5 +9,23 @@ int main() {
     } else {
         println("i is lower than 5");
     }
+
+    // switch statements
+    enum class Mode {Default, Custom, Standard};
+    Mode mode {Mode::Standard};
+    int value {};
+    switch (mode) {
+        using enum Mode;
+        case Default: 
+            value = 20;
+            break;
+        case Custom:
+            value = 44;
+            break;
+        case Standard:
+            value = 25;
+            break;
+    } println("value : {}",value);
+
     return 0;
 }
