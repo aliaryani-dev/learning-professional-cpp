814b5528db46439409952837755832f66bf5c373
Ali Aryani <lenesis@proton.me>
2026-04-24 08:06:18 +0330

    if else conditions

---
+++ 00008-conditional-statements/main.cpp
@@ -0,0 +1,13 @@
+#include <print>
+using namespace std;
+int main() {
+    // if else statements
+    if (int i {8}; i > 5) {
+        println("i is bigger than 5");
+    } else if (i == 5) {
+        println("i is equal to 5");
+    } else {
+        println("i is lower than 5");
+    }
+    return 0;
+}
