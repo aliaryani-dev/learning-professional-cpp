418b74de620410dcff56d874758e0c7d54df4eb4
Ali Aryani <lenesis@proton.me>
2026-04-25 17:49:17 +0330

    return int

--- 00010-functions/main.cpp
+++ 00010-functions/main.cpp
@@ -6,7 +6,12 @@ void my_function (int num, char c) {
     println ("character is : {}",c);
 }
 
+int add_numbers (int a, int b) {
+    return a + b;
+}
+
 auto main() -> int {
     my_function(12, 'b');
+    println("12 + 44 = {}",add_numbers(12,44));
     return 0;
 }
