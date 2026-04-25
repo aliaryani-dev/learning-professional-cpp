99a29b34b5fccac5e09bcb8569720eb6a1ce1657
Ali Aryani <lenesis@proton.me>
2026-04-25 18:09:59 +0330

    function overloading

--- 00010-functions/main.cpp
+++ 00010-functions/main.cpp
@@ -10,9 +10,14 @@ int add_numbers (int a, int b) {
     println("Entering function {}", __func__);
     return a + b;
 }
+// add_numbers for double type : (this is called function overloading)
+double add_numbers (double a, double b) {
+    return a + b;
+}
 
 auto main() -> int {
     my_function(12, 'b');
     println("12 + 44 = {}",add_numbers(12,44));
+    println("11.77 + 13.92 = {}",add_numbers(11.77,13.92));
     return 0;
 }
