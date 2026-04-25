034a576ca4057c6641164531b105cd2bf0643f44
Ali Aryani <lenesis@proton.me>
2026-04-25 22:18:25 +0330

    add new items to vector

--- 00012-vectors/main.cpp
+++ 00012-vectors/main.cpp
@@ -1,7 +1,10 @@
+#include <print>
 #include <vector>
 using namespace std;
 
 auto main() -> int {
     vector<int> my_vector {3,5,7};
-    
+    my_vector.push_back(11);
+    my_vector.push_back(13);
+    println("my_vector 3rd item is : {}",my_vector[3]);
 }
