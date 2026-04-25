91e6f0d5656fe2fcfc2d259bc134cd60665069bc
Ali Aryani <lenesis@proton.me>
2026-04-25 22:25:19 +0330

    declare and use pairs

---
+++ 00013-pairs/main.cpp
@@ -0,0 +1,8 @@
+#include <print>
+#include <utility>
+using namespace std;
+
+auto main() -> int {
+    pair<double, int> my_pair {11.32, 12};
+    println("first item is {} and the second one is {}",my_pair.first,my_pair.second);
+}
