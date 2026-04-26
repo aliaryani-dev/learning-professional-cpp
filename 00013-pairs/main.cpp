81be09907812b520ff357e998c416047677fa384
Ali Aryani <lenesis@proton.me>
2026-04-26 11:41:06 +0330

    format

--- 00013-pairs/main.cpp
+++ 00013-pairs/main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 auto main() -> int {
     pair<double, int> my_pair {11.32, 12};
-    println("first item is {} and the second one is {}",my_pair.first,my_pair.second);
+    println("first item is {} and the second one is {}",
+        my_pair.first,my_pair.second);
 }
