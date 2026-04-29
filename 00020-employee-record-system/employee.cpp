a3c6be8e820a0abbd2c2440dd4d970fefce806b4
Ali Aryani <lenesis@proton.me>
2026-04-29 21:52:43 +0330

    declare promote()

--- 00020-employee-record-system/employee.cpp
+++ 00020-employee-record-system/employee.cpp
@@ -5,4 +5,8 @@ using namespace std;
 namespace Records {
     Employee::Employee (const string& first_name, const string& last_name)
         : m_first_name {first_name}, m_last_name {last_name} {}
+    
+    void Employee::promote (int raise_amount) {
+        set_salary (get_salary() + raise_amount);
+    }
 }
