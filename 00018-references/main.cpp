3197e4b413e6c4e1df9b9606a3dcc7e02d8177e3
Ali Aryani <lenesis@proton.me>
2026-04-29 11:36:50 +0330

    ref to const

--- 00018-references/main.cpp
+++ 00018-references/main.cpp
@@ -5,7 +5,13 @@ auto main() -> int {
     int x {5};
     int& x_ref {x}; // ref variables must always be initialised
     println("x before y : {}",x);
+
     int y {4};
     x_ref = y; // changes the value of x to 4, does not change the ref
     println("x after y : {}",x);
+
+    // ref to const
+    int z {3};
+    const int& z_ref {z};
+    // z_ref = 4; //! error
 }
