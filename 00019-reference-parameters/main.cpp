1df6d9216d21fb513e9a61be77aa6adf8a18d032
Ali Aryani <lenesis@proton.me>
2026-04-29 13:03:52 +0330

    declare add_one_II() using refs

--- 00019-reference-parameters/main.cpp
+++ 00019-reference-parameters/main.cpp
@@ -5,9 +5,15 @@ void add_one (int i) {
     i++;
 }
 
+void add_one_II (int& i) {
+    i++;
+}
+
 auto main() -> int {
     int a {50};
     println("a before calling add_one() : {}",a);
     add_one(a);
     println("a after calling add_one() : {}",a);
+    add_one_II (a);
+    println("a after calling add_one_II() : {}",a);
 }
