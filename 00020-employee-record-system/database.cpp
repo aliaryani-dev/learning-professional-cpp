b1b8b9a90f3c8a49eb60adc3d1d899b037fd26bc
Ali Aryani <lenesis@proton.me>
2026-04-30 08:33:42 +0330

    declare get_employee()

--- 00020-employee-record-system/database.cpp
+++ 00020-employee-record-system/database.cpp
@@ -10,4 +10,23 @@ namespace Records {
         m_employees.push_back(the_employee);
         return m_employees.back();
     }
+    
+    Employee& Database::get_employee (int employee_number) {
+        for (auto& employee : m_employees) {
+            if (employee.get_employee_number() == employee_number) {
+                return employee;
+            }
+        }
+        throw logic_error { "No employees found!"};
+    }
+    Employee& Database::get_employee (const std::string& first_name,
+                                    const std::string& last_name) {
+        for (auto& employee : m_employees) {
+            if (employee.get_first_name() == first_name
+            && employee.get_last_name() == last_name) {
+                return employee;
+            }
+        }
+        throw logic_error { "No employees found!"};
+    }
 }
