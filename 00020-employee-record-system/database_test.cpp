79cdb2d19dce0cc91b96f34102a8e2e72c9f1bbc
Ali Aryani <lenesis@proton.me>
2026-04-30 08:47:37 +0330

    imports; declare 3 employees for test

---
+++ 00020-employee-record-system/database_test.cpp
@@ -0,0 +1,18 @@
+import database;
+import <print>;
+
+using namespace std;
+using namespace Records;
+
+auto main() -> int {
+    Database my_db;
+    Employee& emp1 {my_db.add_employee ("Greg", "Wallis")};
+    emp1.fire();
+
+    Employee& emp2 {my_db.add_employee ("Marc", "White")};
+    emp2.set_salary (100'000);
+    
+    Employee& emp3 {my_db.add_employee ("John", "Doe")};
+    emp3.set_salary (100'000);
+    emp3.promote();
+}
